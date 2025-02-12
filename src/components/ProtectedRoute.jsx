f86997d79228791e866e1fb661891e8eb44a9a60
ana giulia <anaazanedo09@gmail.com>
2025-02-12 16:06:13 -0500

    web_project_around_auth

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { currentuserContext } from "../contexts/CurrentUserContext";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute() {
+export default function ProtectedRoute({ children }) {
   const currentUser = useContext(currentuserContext);
   console.log(currentUser);
   if (currentUser.id) {
-    return "children";
+    return children;
   } else {
     return <Navigate to="/login" />;
   }
